3b2b3696cf6e00a5a55277024ec2a47757a4d7b4
teoMarinov <teodor.b.marinov@gmail.com>
2025-05-08 13:23:53 +0300

    remove temp counter

--- apollo-client/index.ts
+++ apollo-client/index.ts
@@ -1,8 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-let counter = 0;
+
 const createApolloClient = () => {
-  counter++;
-  console.log(counter);
   return new ApolloClient({
     uri: process.env.NEXT_PUBLIC_BACKEND_URL,
     cache: new InMemoryCache(),
