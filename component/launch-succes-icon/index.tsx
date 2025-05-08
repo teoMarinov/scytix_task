265707e6d81dca7b1c7cf26a6a41fac6cf9af10d
teoMarinov <teodor.b.marinov@gmail.com>
2025-05-08 16:09:57 +0300

    Change size to 25

--- component/launch-succes-icon/index.tsx
+++ component/launch-succes-icon/index.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 
-export default function LaunchSuccessIcon({ success }: { success: boolean }) {
+export default function LaunchSuccessIcon({
+  success,
+}: {
+  success: boolean;
+}) {
   const imageAlt = success ? "Launch Failed" : "Launch Succeeded";
   const imagePath = success ? "/success-icon.svg" : "/error-icon.svg";
 
-  return <Image height={20} width={20} src={imagePath} alt={imageAlt} />;
+  return <Image height={25} width={25} src={imagePath} alt={imageAlt} />;
 }
