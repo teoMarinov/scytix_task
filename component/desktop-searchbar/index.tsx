fcb77c236ef51e103e3bf691e8b564395000e7ee
teoMarinov <teodor.b.marinov@gmail.com>
2025-05-08 18:56:44 +0300

    fix unwanted redicrections from header desktop input

--- component/desktop-searchbar/index.tsx
+++ component/desktop-searchbar/index.tsx
@@ -3,17 +3,23 @@
 import { useDebounce } from "use-debounce";
 import { useState, useEffect } from "react";
 import { Paper, InputBase } from "@mui/material";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
 export default function DesktopSearchbar() {
   const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
 
   const [input, setInput] = useState(searchParams.get("mission_name") || "");
   const [debouncedInput] = useDebounce(input, 500);
 
   useEffect(() => {
+    setInput(searchParams.get("mission_name") || "");
+  }, [searchParams]);
+
+  useEffect(() => {
     const params = new URLSearchParams(Array.from(searchParams.entries()));
+    const page = params.get("page");
 
     if (debouncedInput) {
       params.set("mission_name", debouncedInput);
@@ -21,8 +27,16 @@ export default function DesktopSearchbar() {
       params.delete("mission_name");
     }
 
-    router.push(`/?${params.toString()}`);
-  }, [debouncedInput, router, searchParams]);
+    if (page) {
+      params.delete("page");
+    }
+
+    //Header is always loaded so to prevent redirecting to '/' when changing the route
+    // while allowing for the input to redicret to home after using it we use this check
+    if (pathname === "/" || params.has("mission_name"))
+      router.push(`/?${params.toString()}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedInput]);
 
   return (
     <Paper
